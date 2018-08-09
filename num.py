2ff6a803359e80e9c68355ad96473cb74d69e612
Bhumireddysravani <41771061+Bhumireddysravani@users.noreply.github.com>
2018-08-09 15:42:32 +0530

    Update num.py

--- num.py
+++ num.py
@@ -2,8 +2,8 @@ num1=2
 num2=4
 num3=6
 if((num1>num2) and (num1>num3)):
-	print num1
+	print ("num1")
 elif((num2>num1) and (num2>num3)):
-	print num2
+	print ("num2")
 else:
-	print num3
+	print ("num3")
