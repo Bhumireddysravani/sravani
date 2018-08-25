dc7f7a764506563452084d7f4a67f01531441d7d
Bhumireddysravani <41771061+Bhumireddysravani@users.noreply.github.com>
2018-08-25 10:42:26 +0530

    Update pgm36.py

--- pgm36.py
+++ pgm36.py
@@ -1,3 +1,3 @@
 import string
-c = list(''.join(map(str,range(10))) + string.ascii_lowercase + ' ')
-print len([i for i in raw_input().lower() if i not in c])
+b = list(''.join(map(str,range(10))) + string.ascii_lowercase + ' ')
+print len([i for i in raw_input().lower() if i not in b])
